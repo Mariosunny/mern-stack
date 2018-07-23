ff0d0ac2f109fdb0e56e984f397885d4b4fd87e2
Tyler Hurson <thurson@securelink.com>
2018-07-23 14:08:49 -0500

    Implemented mongodb

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongo = require('mongodb');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+var MongoClient = mongo.MongoClient;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +21,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
+MongoClient.connect("mongodb://localhost:27017", function(error, client) {
+
+  if(!error) {
+
+    console.log("Database connected successfully.");
+  }
+
+  app.locals.db = client.db('students');
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.get('/', function(req, res, next) {
-  res.render('index');
+
+  const db = req.app.locals.db;
+
+  db.collection('students').find({}).toArray(function(error, result) {
+    console.log(result);
+    res.render('index', {students: result});
+  });
+});
+
+router.post('/add', function(req, res, next) {
+
+  const db = req.app.locals.db;
+
+  db.collection('students').insertOne({
+    name: req.body.name,
+    age: +req.body.age
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
